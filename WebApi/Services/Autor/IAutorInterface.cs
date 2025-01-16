5db768f81276c4ea7b9c1d9c218f65a15d458115
GuilhermeHMG <guilherme.ghg00@gmail.com>
2025-01-15 23:50:17 -0300

    Initial commit

---
+++ WebApi/Services/Autor/IAutorInterface.cs
@@ -0,0 +1,12 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services.Autor
+{
+    public interface IAutorInterface
+    {
+        Task<ResponseModel<List<AutorModel>>> ListarAutores();
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor);
+        Task<ResponseModel<List<AutorModel>>> BuscarAutorPorIdLivro(int idLivro);
+
+    }
+}
